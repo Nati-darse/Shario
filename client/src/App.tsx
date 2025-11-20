604fb5011980f38ef5f72cfcc984427a1a783f93
Nati-darse <natnaeldarsema@gmail.com>
2025-11-19 16:04:16 -0800

    start from landing page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,29 +1,24 @@
+// client/src/App.tsx
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { AuthProvider } from './contexts/AuthContext';
 import Navbar from './components/layout/Navbar';
 import Home from './pages/Home';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import Dashboard from './pages/Dashboard';
+// import Resources from './pages/Resources';
 
 function App() {
   return (
-    <AuthProvider>
-      <Router>
-        <div className="min-h-screen bg-gray-50">
-          <Navbar />
-          <main>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/register" element={<Register />} />
-              <Route path="/dashboard" element={<Dashboard />} />
-            </Routes>
-          </main>
-        </div>
-      </Router>
-    </AuthProvider>
+    <Router>
+      <div className="min-h-screen bg-gray-50">
+        <Navbar />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            {/* <Route path="/resources" element={<Resources />} /> */}
+          </Routes>
+        </main>
+      </div>
+    </Router>
   );
 }
+
 export default App;
